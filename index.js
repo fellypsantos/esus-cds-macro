8588b32d0e274ed3589830c27bc91452794cc0ed
Fellyp <fellypsantos2011@gmai.com>
2020-02-21 13:28:31 -0300

    Removida autodigitação de nome de cidade.

--- index.js
+++ index.js
@@ -11,7 +11,6 @@
 const computer = require('os').userInfo().username;
 const express = require('express');
 const axios = require('axios');
-const execute = require('child_process').execFile;
 const cors = require('cors');
 const server = express().use(cors());
 const queryServerIP = '192.168.1.111';
@@ -27,18 +26,6 @@ server.get('/get/:cns', async (req, res) => {
   console.log('Resposta: ', response);
 
   res.send(response);
-});
-
-server.get('/type/:city', async (req, res) => {
-
-  let { city } = req.params;
-
-  execute( 'script.exe', [ city ] , (err, data) => {
-    if (err) { console.log(err); }
-    console.log( `[DIGITADO] ${data.toString()}\n` );
-  });
-
-  return res.json({ typed: city });
 });
 
 server.listen(5432, () => {
